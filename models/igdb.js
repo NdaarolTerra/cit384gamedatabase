e025bd6e6f032c78c45e61e0516eef5e2690706b
NdaarolTerra <terrabyte1214@gmail.com>
2023-12-15 00:36:25 -0800

    successfully authenticate igdb

--- models/igdb.js
+++ models/igdb.js
@@ -1,24 +1,18 @@
-//client secret: 73e8glvmrl55z9q7ckwhj7wdo9tplz
-//client ID: 93ug1degpxnaga64g466c83nkeg6tv
-//URL:
-
 const axios = require('axios');
 
-const clientId = '93ug1degpxnaga64g466c83nkeg6tvD'; // Replace with your actual Client ID
-const clientSecret = '73e8glvmrl55z9q7ckwhj7wdo9tplz'; // Replace with your actual Client Secret
+const clientId = '93ug1degpxnaga64g466c83nkeg6tv';
+const clientSecret = 'mpi97u9ibmbc6e5vzznpdo47vcoypz';
 
 async function getAccessToken() {
+    const url = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`;
+
     try {
-        const url = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`;
-        const response = await axios.post(url, null);
-        const accessToken = response.data.access_token;
-        console.log('Access Token:', accessToken);
-        return accessToken;
+        const response = await axios.post(url);
+        return response.data;
     } catch (error) {
         console.error('Error fetching access token:', error);
-        return null;
+        throw error;
     }
 }
 
-// Call the function to test it
-getAccessToken();
+module.exports = {getAccessToken};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('axios');
+const igdb = require('./models/igdb.js');
 const app = express();
 
 
@@ -8,9 +8,20 @@ app.get('/', (req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
 
+// Routes here VVVV
+// Authenticate with IGDB API Route
+app.get('/authenticate', async (req, res) => {
+  try {
+      const tokenResponse = await igdb.getAccessToken();
+      console.log('Token Response', tokenResponse);
+      res.json({ tokenResponse });
+  } catch (error) {
+      console.error('Authentication error:', error);
+      res.status(500).send('Authentication failed');
+  }
+});
 
 const port = 3000;
-
 app.listen(port, () => {
     console.log(`Started server on http://localhost:${port}`);
   });
